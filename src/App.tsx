423b99599853b976419d5058c0dc62d821919f82
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-01 21:06:20 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import {
   createRoutesFromElements,
   createBrowserRouter,
   Route,
+  Navigate,
 } from "react-router-dom";
 import MainPage from "./pages/mainPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<Navigate to="/companies/organizations" replace />} />
+      <Route path="/companies" element={<Navigate to="/companies/organizations" replace />} />
+      <Route path="/companies/organizations" element={<MainPage />} />
+      <Route path="/companies/contractors" element={<MainPage />} />
+      <Route path="/companies/clients" element={<MainPage />} />
+      <Route path="/search" element={<MainPage />} />
+      <Route path="/settings" element={<MainPage />} />
+      <Route path="/signout" element={<MainPage />} />
     </Route>
   )
 );
