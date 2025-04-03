128600c69f1605ce5c8ccb767aef22dcd324429d
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-03 22:57:11 +0300

    add icons

---
+++ src/components/icons/cancelIcon.tsx
@@ -0,0 +1,10 @@
+const CancelIcon = () => {
+    return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 3.5L3.5 12.5" stroke="#3B3B3B" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12.5 12.5L3.5 3.5" stroke="#3B3B3B" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
+
+export default CancelIcon;
---
+++ src/components/icons/chevronIconLeft.tsx
@@ -0,0 +1,16 @@
+const ChevronIconLeft = () => {
+    return (
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_12740_407)">
+                <path d="M13 4L7 10L13 16" stroke="#3B3B3B" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_12740_407">
+                    <rect width="20" height="20" fill="white"/>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export default ChevronIconLeft;
