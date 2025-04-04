34192b90c61fce32f80dc777cd1862f134ebc13e
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-04 13:40:15 +0300

    add modal provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ModalProvider } from "./utils/modalProvider";
 import router from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ModalProvider>
+      <RouterProvider router={router} />
+    </ModalProvider>
   </React.StrictMode>
 );
